5713b05a40a11983ff31263acdc9300de9ea644c
InfinityZ25 <henixceo@gmail.com>
2019-08-05 16:02:14 -0400

    Initial Commit 2.0

---
+++ src/main/java/me/infinityz/NoobstersArenaBungeeModule.java
@@ -0,0 +1,15 @@
+package me.infinityz;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * NoobstersArenaBungeeModule
+ */
+public class NoobstersArenaBungeeModule extends Plugin{
+    @Override
+    public void onEnable(){
+        getLogger().info("Loads");
+    }
+
+    
+}
