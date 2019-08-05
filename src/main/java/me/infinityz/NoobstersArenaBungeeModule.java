484b3e3f07fe66fc6f1a92ab8c8c573f2394eec4
InfinityZ25 <henixceo@gmail.com>
2019-08-05 17:00:05 -0400

    Adding dependecies to pom and making it compileable

--- src/main/java/me/infinityz/NoobstersArenaBungeeModule.java
+++ src/main/java/me/infinityz/NoobstersArenaBungeeModule.java
@@ -2,9 +2,6 @@ package me.infinityz;
 
 import net.md_5.bungee.api.plugin.Plugin;
 
-/**
- * NoobstersArenaBungeeModule
- */
 public class NoobstersArenaBungeeModule extends Plugin{
     @Override
     public void onEnable(){
